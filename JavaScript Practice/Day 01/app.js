9e79040c6b96bca63e81a47408cdea62d5184716
annasshah <anasshahskype@gmail.com>
2022-01-13 22:27:11 +0500

    JS practice 1 - 10

---
+++ JavaScript Practice/Day 01/app.js
@@ -0,0 +1,140 @@
+// alert
+// window.alert('hello world!')
+
+// Variables for Strings
+// var name;
+// name = 'Mark'
+// var newName = "john"
+// var newname = 'smith'
+// alert(name1)
+
+// Variables for Numbers
+// var weight;
+// weight = 45 + 3
+
+// var num1 = 10
+// var num2  = 15
+// var number3 = num1 + num2 
+
+
+// Variable Names Legal and Illegal
+
+// var 1num;
+// var num 1;
+// var num.
+// var num.new
+
+// var num
+// var number
+// var newNumber  
+// var _number
+// var $number
+// var number
+// var number2
+// var number 
+// var numbeR 
+
+
+
+// alert(number3)
+
+
+// Math expressions:
+// Familiar operators
+
+
+// var number1 = 4
+// var number2 = 6
+
+// var sum = 12 / 4
+// var sum = 10 % 4
+// alert (sum)
+
+
+// Math expressions:
+// Unfamiliar operators
+
+var num = 5
+
+// pre-Increment
+// ++num1
+
+// post-Increment
+// num1++
+
+// console.log(num1++);
+// console.log(num1);
+
+
+// pre-Decrement
+// --num1
+
+// post-Decrement
+// num1--
+
+// console.log(num1--);
+// console.log(num1);
+
+
+// var sum  = num + num++ + --num + num - --num
+// console.log(sum);
+//             // 5  + 5  +  5  +  5 - 4
+
+
+
+// Math expressions:
+// Eliminating ambiguity
+
+// var totalCost = 1 + 3 * 4
+
+// var totalCost = (1 + 3) * 4
+// var totalCost = 1 + (3 * 4)
+
+// var totalCost = (2 * 4) * 4 + 2
+                    // 8 * 4 + 2
+                    // 32 + 2
+                    // 34
+
+// var totalCost = (2 * 4) * (4 + 2)
+                // 8 * 6
+                // 48
+// console.log(totalCost);
+
+
+
+// Concatenating text strings
+
+// var name = 'Smith'
+// var message = 'Thanks ' + name + ' for your input!'
+
+// var value = '22' + 22
+// var val1 =  22 + 25 + '22'
+// var newVal =  "2 plus 2 equals " + 2 + 2
+// console.log(newVal);
+
+
+// Prompts
+
+// var userName = prompt('Enter Your Name','Your Name')
+
+// alert('Good Morning ' + userName + '!')
+
+// console.log(userName);
+
+
+// if statements
+
+// var score = 0
+
+// var x = prompt("Where does the Pope live?");
+
+// if(x == "Vatican"){
+//     alert('Correct Answer!')
+//     score++
+
+//     console.log(score);
+// }
+
+
+
+
